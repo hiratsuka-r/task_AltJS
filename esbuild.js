4d7511213db9eb6c2784047142ae995e3b39a671
hiratsuka-r <22903625+hiratsuka-r@users.noreply.github.com>
2023-01-27 13:04:52 +0900

    add scss

--- esbuild.js
+++ esbuild.js
@@ -1,11 +1,14 @@
 const { build } = require('esbuild');
+const { sassPlugin } = require('esbuild-sass-plugin');
+
 const glob = require('glob');
 const entryPoints = glob.sync('./src/**/*.{ts,jsx,js}');
 
 build({
   entryPoints,
   outdir: 'docs/build/',
-  bundle: true
+  bundle: true,
+  plugins: [sassPlugin()]
 }).then((result) => {
   console.log(`ビルド完了`);
 }).catch(() => {
